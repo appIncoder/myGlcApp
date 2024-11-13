00d5eb4a6fe74b9faa5e942651d07b6050c063cd
appIncoder <46226243+appIncoder@users.noreply.github.com>
2024-11-13 17:17:01 +0100

    first commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'be.glcbaudour.app.mobile',
+  appName: 'myGLCApp',
+  webDir: 'www'
+};
+
+export default config;
